7f8b7b868a70ff6b7a43bc1a42e3728bcd91598a
milan460 <patel.milan416@gmail.com>
2024-08-30 20:58:11 -0400

    adding the StockPrice CRUD functions

--- stockservice/src/main/java/com/skillstorm/stockservice/models/StockPrice.java
+++ stockservice/src/main/java/com/skillstorm/stockservice/models/StockPrice.java
@@ -1,4 +1,45 @@
 package com.skillstorm.stockservice.models;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
 public class StockPrice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "stock_id", nullable = false)
+    private Stock stock;
+    @Column(nullable = false)
+    private BigDecimal openPrice;
+
+    @Column(nullable = false)
+    private BigDecimal highPrice;
+
+    @Column(nullable = false)
+    private BigDecimal lowPrice;
+
+    @Column(nullable = false)
+    private BigDecimal currentPrice;
+
+    @Column(nullable = false)
+    private Long volume;
+
+    @Column(nullable = false)
+    private BigDecimal previousClose;
+
+    @Column(nullable = false)
+    private BigDecimal priceChange;
+
+    @Column(nullable = false)
+    private BigDecimal changePercent;
+
+    @Column(nullable = false)
+    private LocalDate tradingDay;
+
 }
---
+++ stockservice/src/main/java/com/skillstorm/stockservice/services/StockPriceService.java
@@ -0,0 +1,38 @@
+package com.skillstorm.stockservice.services;
+
+import com.skillstorm.stockservice.models.Stock;
+import com.skillstorm.stockservice.models.StockPrice;
+import com.skillstorm.stockservice.repositories.StockPriceRespository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StockPriceService {
+
+    @Autowired
+    private StockPriceRespository stockPriceRespository;
+
+    public List<StockPrice> getAllStockPrices(){
+        return stockPriceRespository.findAll();
+    }
+    public StockPrice getStockPriceById(int id){
+        Optional<StockPrice> stockPrice = stockPriceRespository.findById(id);
+        return stockPrice.get();
+    }
+
+
+    @Transactional
+    public StockPrice save(StockPrice stockPrice) {
+        return stockPriceRespository.save(stockPrice);
+    }
+
+
+    @Transactional
+    public void deleteById(int id) {
+        stockPriceRespository.deleteById(id);
+    }
+}
