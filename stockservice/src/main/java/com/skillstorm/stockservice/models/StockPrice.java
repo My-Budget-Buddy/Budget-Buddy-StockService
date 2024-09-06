7f7fb3704effc1a06e6535ed5d1f660b0434bb7a
alxsfteng <alex.sfteng@gmail.com>
2024-09-06 15:44:23 -0400

    API call

--- stockservice/src/main/java/com/skillstorm/stockservice/models/StockPrice.java
+++ stockservice/src/main/java/com/skillstorm/stockservice/models/StockPrice.java
@@ -42,4 +42,222 @@ public class StockPrice {
     @Column(nullable = false)
     private LocalDate tradingDay;
 
+    public StockPrice() {
+    }
+
+    public StockPrice(Stock stock, BigDecimal openPrice, BigDecimal highPrice, BigDecimal lowPrice,
+            BigDecimal currentPrice, Long volume, BigDecimal previousClose, BigDecimal priceChange,
+            BigDecimal changePercent, LocalDate tradingDay) {
+        this.stock = stock;
+        this.openPrice = openPrice;
+        this.highPrice = highPrice;
+        this.lowPrice = lowPrice;
+        this.currentPrice = currentPrice;
+        this.volume = volume;
+        this.previousClose = previousClose;
+        this.priceChange = priceChange;
+        this.changePercent = changePercent;
+        this.tradingDay = tradingDay;
+    }
+
+    public StockPrice(int id, Stock stock, BigDecimal openPrice, BigDecimal highPrice, BigDecimal lowPrice,
+            BigDecimal currentPrice, Long volume, BigDecimal previousClose, BigDecimal priceChange,
+            BigDecimal changePercent, LocalDate tradingDay) {
+        this.id = id;
+        this.stock = stock;
+        this.openPrice = openPrice;
+        this.highPrice = highPrice;
+        this.lowPrice = lowPrice;
+        this.currentPrice = currentPrice;
+        this.volume = volume;
+        this.previousClose = previousClose;
+        this.priceChange = priceChange;
+        this.changePercent = changePercent;
+        this.tradingDay = tradingDay;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Stock getStock() {
+        return stock;
+    }
+
+    public void setStock(Stock stock) {
+        this.stock = stock;
+    }
+
+    public BigDecimal getOpenPrice() {
+        return openPrice;
+    }
+
+    public void setOpenPrice(BigDecimal openPrice) {
+        this.openPrice = openPrice;
+    }
+
+    public BigDecimal getHighPrice() {
+        return highPrice;
+    }
+
+    public void setHighPrice(BigDecimal highPrice) {
+        this.highPrice = highPrice;
+    }
+
+    public BigDecimal getLowPrice() {
+        return lowPrice;
+    }
+
+    public void setLowPrice(BigDecimal lowPrice) {
+        this.lowPrice = lowPrice;
+    }
+
+    public BigDecimal getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(BigDecimal currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
+    public Long getVolume() {
+        return volume;
+    }
+
+    public void setVolume(Long volume) {
+        this.volume = volume;
+    }
+
+    public BigDecimal getPreviousClose() {
+        return previousClose;
+    }
+
+    public void setPreviousClose(BigDecimal previousClose) {
+        this.previousClose = previousClose;
+    }
+
+    public BigDecimal getPriceChange() {
+        return priceChange;
+    }
+
+    public void setPriceChange(BigDecimal priceChange) {
+        this.priceChange = priceChange;
+    }
+
+    public BigDecimal getChangePercent() {
+        return changePercent;
+    }
+
+    public void setChangePercent(BigDecimal changePercent) {
+        this.changePercent = changePercent;
+    }
+
+    public LocalDate getTradingDay() {
+        return tradingDay;
+    }
+
+    public void setTradingDay(LocalDate tradingDay) {
+        this.tradingDay = tradingDay;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + ((stock == null) ? 0 : stock.hashCode());
+        result = prime * result + ((openPrice == null) ? 0 : openPrice.hashCode());
+        result = prime * result + ((highPrice == null) ? 0 : highPrice.hashCode());
+        result = prime * result + ((lowPrice == null) ? 0 : lowPrice.hashCode());
+        result = prime * result + ((currentPrice == null) ? 0 : currentPrice.hashCode());
+        result = prime * result + ((volume == null) ? 0 : volume.hashCode());
+        result = prime * result + ((previousClose == null) ? 0 : previousClose.hashCode());
+        result = prime * result + ((priceChange == null) ? 0 : priceChange.hashCode());
+        result = prime * result + ((changePercent == null) ? 0 : changePercent.hashCode());
+        result = prime * result + ((tradingDay == null) ? 0 : tradingDay.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        StockPrice other = (StockPrice) obj;
+        if (id != other.id)
+            return false;
+        if (stock == null) {
+            if (other.stock != null)
+                return false;
+        } else if (!stock.equals(other.stock))
+            return false;
+        if (openPrice == null) {
+            if (other.openPrice != null)
+                return false;
+        } else if (!openPrice.equals(other.openPrice))
+            return false;
+        if (highPrice == null) {
+            if (other.highPrice != null)
+                return false;
+        } else if (!highPrice.equals(other.highPrice))
+            return false;
+        if (lowPrice == null) {
+            if (other.lowPrice != null)
+                return false;
+        } else if (!lowPrice.equals(other.lowPrice))
+            return false;
+        if (currentPrice == null) {
+            if (other.currentPrice != null)
+                return false;
+        } else if (!currentPrice.equals(other.currentPrice))
+            return false;
+        if (volume == null) {
+            if (other.volume != null)
+                return false;
+        } else if (!volume.equals(other.volume))
+            return false;
+        if (previousClose == null) {
+            if (other.previousClose != null)
+                return false;
+        } else if (!previousClose.equals(other.previousClose))
+            return false;
+        if (priceChange == null) {
+            if (other.priceChange != null)
+                return false;
+        } else if (!priceChange.equals(other.priceChange))
+            return false;
+        if (changePercent == null) {
+            if (other.changePercent != null)
+                return false;
+        } else if (!changePercent.equals(other.changePercent))
+            return false;
+        if (tradingDay == null) {
+            if (other.tradingDay != null)
+                return false;
+        } else if (!tradingDay.equals(other.tradingDay))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "StockPrice [id=" + id + ", stock=" + stock + ", openPrice=" + openPrice + ", highPrice=" + highPrice
+                + ", lowPrice=" + lowPrice + ", currentPrice=" + currentPrice + ", volume=" + volume
+                + ", previousClose=" + previousClose + ", priceChange=" + priceChange + ", changePercent="
+                + changePercent + ", tradingDay=" + tradingDay + ", getClass()=" + getClass() + ", getId()=" + getId()
+                + ", getStock()=" + getStock() + ", getOpenPrice()=" + getOpenPrice() + ", getHighPrice()="
+                + getHighPrice() + ", getLowPrice()=" + getLowPrice() + ", getCurrentPrice()=" + getCurrentPrice()
+                + ", getVolume()=" + getVolume() + ", getPreviousClose()=" + getPreviousClose() + ", getPriceChange()="
+                + getPriceChange() + ", getChangePercent()=" + getChangePercent() + ", getTradingDay()="
+                + getTradingDay() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
+    }
+
+    
 }
--- stockservice/src/main/java/com/skillstorm/stockservice/services/StockService.java
+++ stockservice/src/main/java/com/skillstorm/stockservice/services/StockService.java
@@ -1,49 +1,100 @@
 package com.skillstorm.stockservice.services;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.skillstorm.stockservice.models.Stock;
+import com.skillstorm.stockservice.models.StockDataResponse;
+import com.skillstorm.stockservice.models.StockPrice;
+import com.skillstorm.stockservice.repositories.StockPriceRespository;
 import com.skillstorm.stockservice.repositories.StockRepository;
 
 @Service
 public class StockService {
-    
+
+    private static final String API_KEY = "myKey";
+    private static final String API_URL = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=%s&apikey="
+            + API_KEY;
+
+    @Autowired
+    private StockPriceRespository stockPriceRespository;
+
     @Autowired
     private StockRepository stockRepository;
 
-    public List<Stock> getAllStocks(){
+    public List<Stock> getAllStocks() {
         return stockRepository.findAll();
     }
 
-    public Stock getStockById(int id){
+    public Stock getStockById(int id) {
         Optional<Stock> stock = stockRepository.findById(id);
         return stock.orElse(null);
     }
 
-    public Stock updateStock(int id, Stock updatedStock){
+    public Stock updateStock(int id, Stock updatedStock) {
         Optional<Stock> existingStock = stockRepository.findById(id);
-        if(existingStock.isPresent()){
+        if (existingStock.isPresent()) {
             Stock stock = existingStock.get();
             stock.setSymbol(updatedStock.getSymbol());
             stock.setCompanyName(updatedStock.getCompanyName());
             return stockRepository.save(stock);
-        } else{
+        } else {
             return null;
         }
     }
 
-    public Stock createStock(Stock stock){
+    public Stock createStock(Stock stock) {
         return stockRepository.save(stock);
     }
 
-    public void deleteStock(int id) throws Exception{
-        if(!stockRepository.existsById(id)){
+    public void deleteStock(int id) throws Exception {
+        if (!stockRepository.existsById(id)) {
             throw new Exception("Stock with id: " + id + " not found");
         }
         stockRepository.deleteById(id);
     }
+
+    public void updateStockData(String symbol) throws Exception{
+        RestTemplate restTemplate = new RestTemplate();
+        String url = String.format(API_URL, symbol);
+
+        StockDataResponse response = restTemplate.getForObject(url, StockDataResponse.class);
+        if (response != null && response.getGlobalQuote() != null) {
+            Map<String, String> quoteData = response.getGlobalQuote();
+
+            Stock stock = stockRepository.findBySymbol(symbol);
+
+            if (stock == null) {
+                stock = new Stock();
+                stock.setSymbol(symbol);
+                stockRepository.save(stock);
+            }
+
+            StockPrice stockPrice = new StockPrice();
+            stockPrice.setStock(stock);
+            stockPrice.setOpenPrice(new BigDecimal(quoteData.get("02. open")));
+            stockPrice.setHighPrice(new BigDecimal(quoteData.get("03. high")));
+            stockPrice.setLowPrice(new BigDecimal(quoteData.get("04. low")));
+            stockPrice.setCurrentPrice(new BigDecimal(quoteData.get("05. price")));
+            stockPrice.setVolume(Long.parseLong(quoteData.get("06. volume")));
+            stockPrice.setPreviousClose(new BigDecimal(quoteData.get("08. previous close")));
+            stockPrice.setPriceChange(new BigDecimal(quoteData.get("09. change")));
+            stockPrice.setChangePercent(new BigDecimal(quoteData.get("10. change percent").replace("%", "")));
+            stockPrice.setTradingDay(LocalDate.parse(quoteData.get("07. latest trading day")));
+
+            stockPriceRespository.save(stockPrice);
+        } else {
+            throw new Exception("Error fetching stock data from API for symbol: " + symbol);
+        }
+    }
+
 }
